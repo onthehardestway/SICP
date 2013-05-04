717eda0006008e036f9085c88df995aeb2e8d4bb
onthehardestway <onthehardestway@gmail.com>
2013-05-04 22:51:30 +0800

    Exercise 1.6
    why if needs to be provided as a special form

---
+++ Exercise1.6.scm
@@ -0,0 +1,29 @@
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+	(else else-clause)))
+
+(new-if (= 2 3) 0 5)
+(new-if (= 1 1) 0 5)
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(sqrt 2)
+
+;Aborting!: maximum recursion depth exceeded
+;When calling new-if, the argument (sqrt-iter (improve guess x) x) will be evaluated,
+;which will call itself again and again, until stack overflow.
